e9667ee75f6a1676b206192d4bba18005f4f6dff
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-12-16 20:38:10 -0500

    update resource namespaces to support LUS changes (#30)
    (cherry picked from commit 8f71672901987bc3dbf6256e64e228db36a686f5)

--- OTRExporter/VersionInfo.cpp
+++ OTRExporter/VersionInfo.cpp
@@ -8,19 +8,19 @@ void InitVersionInfo()
 {
 	resourceVersions = std::map<uint32_t, uint32_t> {
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Animation), 0 },
-	{ static_cast<uint32_t>(LUS::ResourceType::Texture), 0 },
+	{ static_cast<uint32_t>(Fast::ResourceType::Texture), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_PlayerAnimation), 0 },
-	{ static_cast<uint32_t>(LUS::ResourceType::DisplayList), 0 },
+	{ static_cast<uint32_t>(Fast::ResourceType::DisplayList), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Room), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_CollisionHeader), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Skeleton), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_SkeletonLimb), 0 },
-	{ static_cast<uint32_t>(LUS::ResourceType::Matrix), 0 },
+	{ static_cast<uint32_t>(Fast::ResourceType::Matrix), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Path), 0 },
-	{ static_cast<uint32_t>(LUS::ResourceType::Vertex), 0 },
+	{ static_cast<uint32_t>(Fast::ResourceType::Vertex), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Cutscene), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Array), 0 },
 	{ static_cast<uint32_t>(SOH::ResourceType::SOH_Text), 0 },
-	{ static_cast<uint32_t>(LUS::ResourceType::Blob), 0 },
+	{ static_cast<uint32_t>(Ship::ResourceType::Blob), 0 },
 	};
 }
